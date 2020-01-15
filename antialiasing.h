afda5accae874246c7a06447436ed314ad7ff99c
Kurt Moraw <km@flyheli.de>
2020-01-15 16:56:39 +0100

    Delete antialiasing.h

--- antialiasing.h
+++
@@ -1,5 +0,0 @@
-void fir_filter_i_ssb_inc(double sample, int client);
-int fir_filter_i_ssb(double sample, int client);
-
-void fir_filter_q_ssb_inc(double sample, int client);
-int fir_filter_q_ssb(double sample, int client);
