3db0309ed124eedc7a373545bf824ecf52f0b5da
Kurt <km@flyheli.de>
2021-05-15 18:31:40 +0200

    SDRplay API V3

--- sdrplay.c
+++ sdrplay.c
@@ -39,29 +39,39 @@ double lastsdrqrg = 0;
 #include "ssbfft.h"
 #include "wb_fft.h"
 #include "setup.h"
-#include "mirsdrapi-rsp.h"  // the SDRplay driver must be installed !
+#include "sdrplay_api.h"  // the SDRplay driver must be installed !
+
+void EventCallback(sdrplay_api_EventT eventId, sdrplay_api_TunerSelectT tuner,
+sdrplay_api_EventParamsT *params, void *cbContext);
+void StreamACallback(short *xi, short *xq, sdrplay_api_StreamCbParamsT *params, unsigned int numSamples, unsigned int reset, void *cbContext);
+void StreamBCallback(short *xi, short *xq, sdrplay_api_StreamCbParamsT *params, unsigned int numSamples, unsigned int reset, void *cbContext);
 
 int device = 0;     // device number. If we have one SDRplay device connected: device=0
 int devModel = 1;
 float ppmOffset = 0.0;
 int antenna = 0; // 0 = Ant A, 1 = Ant B, 2 = HiZ
-mir_sdr_RSPII_AntennaSelectT ant = mir_sdr_RSPII_ANTENNA_A;
+sdrplay_api_Rsp2_AntennaSelectT ant = sdrplay_api_Rsp2_ANTENNA_A;
 int refClk = 0;
 int notchEnable = 0;
 int biasT = 0;
-mir_sdr_SetGrModeT grMode = mir_sdr_USE_SET_GR_ALT_MODE;
 int gainR = 50;
-#ifdef WIDEBAND
-    int bwkHz = 8000;   // default BW, possible values: 200,300,600,1536,5000,6000,7000,8000
-#else
-    int bwkHz = 5000;   // default BW, possible values: 200,300,600,1536,5000,6000,7000,8000
-#endif // WIDEBAND
 
 int ifkHz = 0;      // 0 is used in this software
 int rspLNA = 0;
 void *cbContext = NULL;
 int agcControl = 1;
 long setPoint = -30;
+sdrplay_api_DeviceT *chosenDevice = NULL;
+sdrplay_api_RxChannelParamsT * chParams;
+sdrplay_api_DeviceT devs[6];
+unsigned int ndev;
+float ver = 0.0;
+sdrplay_api_ErrT err;
+sdrplay_api_DeviceParamsT *deviceParams = NULL;
+sdrplay_api_CallbackFnsT cbFns;
+int reqTuner = 0;
+int master_slave = 0;
+unsigned int chosenIdx = 0;
 
 /*
  * init the SDRplay hardware
@@ -71,93 +81,202 @@ long setPoint = -30;
  */
 int init_SDRplay()
 {
+int i;
+
     printf("Initialize SDRplay hardware\n");
-    mir_sdr_DeviceT devices[4];
-    unsigned int numDevs;
-    int devAvail = 0;
-    mir_sdr_ErrT r;
-    int gRdBsystem;
 
-    // scan for SDRplay devices
-    mir_sdr_GetDevices(&devices[0], &numDevs, 4);
-    
-    // is an SDRplay device available ?
-    for(int i = 0; i < numDevs; i++) 
-    {
-        if(devices[i].devAvail == 1)
-            devAvail++;
-    }
-
-    if (devAvail == 0) {
-        printf("ERROR: No RSP devices available.\n");
+	if ((err = sdrplay_api_Open()) != sdrplay_api_Success)
+	{
+		printf("sdrplay_api_Open failed %s\n", sdrplay_api_GetErrorString(err));
         return 0;
-    }
-
-    if (devices[device].devAvail != 1) {
-        printf("ERROR: RSP selected (%d) is not available.\n", (device + 1));
-        return 0;
-    }
-    
-    // read device information and initialize
-    mir_sdr_SetDeviceIdx(device);
-    devModel = devices[device].hwVer;
-    mir_sdr_SetPpm(ppmOffset);
-
-    // set model specific parameters
-    if (devModel == 2) {
-        if (antenna == 1) {
-            ant = mir_sdr_RSPII_ANTENNA_A;
-            mir_sdr_RSPII_AntennaControl(ant);
-            mir_sdr_AmPortSelect(0);
-        } else {
-            ant = mir_sdr_RSPII_ANTENNA_B;
-            mir_sdr_RSPII_AntennaControl(ant);
-            mir_sdr_AmPortSelect(0);
-        }
-
-        if (antenna == 2) {
-            mir_sdr_AmPortSelect(1);
-        }
-    }
-	
-    grMode = mir_sdr_USE_RSP_SET_GR;
-    if(devModel == 1) grMode = mir_sdr_USE_SET_GR_ALT_MODE;
-
-    double dqrg = tuned_frequency;
-    r = mir_sdr_StreamInit(&gainR, ((double)SDR_SAMPLE_RATE/1e6), dqrg/1e6,
-        (mir_sdr_Bw_MHzT)bwkHz, (mir_sdr_If_kHzT)ifkHz, rspLNA, &gRdBsystem,
-        grMode, &samplesPerPacket, streamCallback, gainCallback, &cbContext);
-
-	if (r != mir_sdr_Success) {
-		printf("Failed to start SDRplay RSP device.\n");
-		return 0;
 	}
-	
-	#ifndef WIDEBAND
-	// narrow band
-	// reduce the sample rate
-	mir_sdr_DecimateControl(1,SR_MULTIPLIER,0);
-    #endif
+    // Check API versions match
+    if ((err = sdrplay_api_ApiVersion(&ver)) != sdrplay_api_Success)
+    {
+        printf("sdrplay_api_ApiVersion failed %s\n", sdrplay_api_GetErrorString(err));
+    }
+    if (ver != SDRPLAY_API_VERSION)
+    {
+        printf("API version don't match (local=%.2f dll=%.2f)\n", SDRPLAY_API_VERSION, ver);
+        sdrplay_api_Close();
+        return 0;
+    }
 
-    mir_sdr_AgcControl(agcControl, setPoint, 0, 0, 0, 0, rspLNA);
-    
-    if (devModel == 2) {
-        if (refClk > 0) {
-            mir_sdr_RSPII_ExternalReferenceControl(1);
-        } else {
-            mir_sdr_RSPII_ExternalReferenceControl(0);
+    sdrplay_api_LockDeviceApi();    // Lock API while device selection is performed
+
+    // Fetch list of available devices
+    if ((err = sdrplay_api_GetDevices(devs, &ndev, sizeof(devs) / sizeof(sdrplay_api_DeviceT))) != sdrplay_api_Success)
+    {
+        printf("sdrplay_api_GetDevices failed %s\n", sdrplay_api_GetErrorString(err));
+        sdrplay_api_UnlockDeviceApi();
+        sdrplay_api_Close();
+        return 0;
+    }
+
+    printf("MaxDevs=%ld NumDevs=%d\n", sizeof(devs) / sizeof(sdrplay_api_DeviceT), ndev);
+
+    if (ndev == 0)
+    {
+        printf("no SDRplay devices found\n");
+        sdrplay_api_UnlockDeviceApi();
+        sdrplay_api_Close();
+        return 0;
+    }
+
+    if (ndev > 0)
+    {
+        for (i = 0; i < (int) ndev; i++)
+        {
+            if (devs[i].hwVer == SDRPLAY_RSPduo_ID)
+                printf("Dev%d: SerNo=%s hwVer=%d tuner=0x%.2x rspDuoMode=0x%.2x\n", i,
+                    devs[i].SerNo, devs[i].hwVer, devs[i].tuner, devs[i].rspDuoMode);
+            else
+                printf("Dev%d: SerNo=%s hwVer=%d tuner=0x%.2x\n", i, devs[i].SerNo,
+                    devs[i].hwVer, devs[i].tuner);
+        }
+        // Choose device
+        if ((reqTuner == 1) || (master_slave == 1))	// requires RSPduo
+        {
+            // Pick first RSPduo
+            for (i = 0; i < (int) ndev; i++)
+            {
+                if (devs[i].hwVer == SDRPLAY_RSPduo_ID)
+                {
+                    chosenIdx = i;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            // Pick first device of any type
+            for (i = 0; i < (int) ndev; i++)
+            {
+                chosenIdx = i;
+                break;
+            }
+        }
+        if (i == ndev)
+        {
+            printf("Couldn't find a suitable device to open - exiting\n");
+            sdrplay_api_UnlockDeviceApi();
+            sdrplay_api_Close();
+            return 0;
+
         }
 
-        if (notchEnable > 0) {
-            mir_sdr_RSPII_RfNotchEnable(1);
-        } else {
-            mir_sdr_RSPII_RfNotchEnable(0);
+        printf("chosenDevice = %d\n", chosenIdx);
+
+        chosenDevice = &devs[chosenIdx];
+        // If chosen device is an RSPduo, assign additional fields
+        if (chosenDevice->hwVer == SDRPLAY_RSPduo_ID)
+        {
+            // If master device is available, select device as master
+            if (chosenDevice->rspDuoMode &sdrplay_api_RspDuoMode_Master)
+            {
+                    // Select tuner based on user input (or default to TunerA)
+                chosenDevice->tuner = sdrplay_api_Tuner_A;
+                if (reqTuner == 1)
+                    chosenDevice->tuner = sdrplay_api_Tuner_B;
+                // Set operating mode
+                if (!master_slave)	// Single tuner mode
+                {
+                    chosenDevice->rspDuoMode = sdrplay_api_RspDuoMode_Single_Tuner;
+                    printf("Dev%d: selected rspDuoMode=0x%.2x tuner=0x%.2x\n", chosenIdx,
+                        chosenDevice->rspDuoMode, chosenDevice->tuner);
+                }
+                else
+                {
+                    chosenDevice->rspDuoMode = sdrplay_api_RspDuoMode_Master;
+                    // Need to specify sample frequency in master/slave mode
+                    chosenDevice->rspDuoSampleFreq = 6000000.0;
+                    printf("Dev%d: selected rspDuoMode=0x%.2x tuner=0x%.2x rspDuoSampleFreq=%.1f\n",
+                        chosenIdx, chosenDevice->rspDuoMode,
+                        chosenDevice->tuner, chosenDevice->rspDuoSampleFreq);
+                }
+            }
+            else	// Only slave device available
+            {
+                    // Shouldn't change any parameters for slave device
+            }
         }
 
-        if (biasT > 0) {
-            mir_sdr_RSPII_BiasTControl(1);
-        } else {
-            mir_sdr_RSPII_BiasTControl(0);
+        // Select chosen device
+        if ((err = sdrplay_api_SelectDevice(chosenDevice)) != sdrplay_api_Success)
+        {
+            printf("sdrplay_api_SelectDevice failed %s\n", sdrplay_api_GetErrorString(err));
+            sdrplay_api_UnlockDeviceApi();
+            sdrplay_api_Close();
+            return 0;
+        }
+
+        sdrplay_api_UnlockDeviceApi();  // Unlock API now that device is selected
+
+        // Retrieve device parameters so they can be changed if wanted
+        if ((err = sdrplay_api_GetDeviceParams(chosenDevice->dev, &deviceParams)) != sdrplay_api_Success)
+        {
+            printf("sdrplay_api_GetDeviceParams failed %s\n", sdrplay_api_GetErrorString(err));
+            sdrplay_api_Close();
+            return 0;
+        }
+        // Check for NULL pointers before changing settings
+        if (deviceParams == NULL)
+        {
+            printf("sdrplay_api_GetDeviceParams returned NULL deviceParams pointer\n");
+            sdrplay_api_Close();
+            return 0;
+        }
+
+        // Configure dev parameters
+        if (deviceParams->devParams != NULL)
+        {
+            printf("======================== SR:%d\n",SDR_SAMPLE_RATE);
+            deviceParams->devParams->fsFreq.fsHz = (double)SDR_SAMPLE_RATE;
+            deviceParams->rxChannelA->ctrlParams.decimation.enable = 1;
+            #ifndef WIDEBAND
+            deviceParams->rxChannelA->ctrlParams.decimation.decimationFactor = SR_MULTIPLIER;
+            #else
+            deviceParams->rxChannelA->ctrlParams.decimation.decimationFactor = 1;
+            #endif
+        }
+
+        // Configure tuner parameters (depends on selected Tuner which parameters to use)
+        chParams = (chosenDevice->tuner == sdrplay_api_Tuner_B) ? deviceParams->rxChannelB :deviceParams->rxChannelA;
+        if (chParams != NULL)
+        {
+            printf("set default QRG: %d\n",tuned_frequency);
+            chParams->tunerParams.rfFreq.rfHz = tuned_frequency;
+            #ifdef WIDEBAND
+            chParams->tunerParams.bwType = sdrplay_api_BW_8_000;
+            #else
+            chParams->tunerParams.bwType = sdrplay_api_BW_1_536;
+            #endif
+            chParams->tunerParams.ifType = sdrplay_api_IF_Zero;
+            chParams->tunerParams.gain.gRdB = 40;
+            chParams->tunerParams.gain.LNAstate = 1;
+            chParams->ctrlParams.agc.enable = sdrplay_api_AGC_DISABLE;
+        }
+        else
+        {
+            printf("sdrplay_api_GetDeviceParams returned NULL chParams pointer\n");
+            sdrplay_api_Close();
+            return 0;
+        }
+
+        // Assign callback functions to be passed to sdrplay_api_Init()
+        cbFns.StreamACbFn = StreamACallback;
+        cbFns.StreamBCbFn = StreamBCallback;
+        cbFns.EventCbFn = EventCallback;
+        // Now we're ready to start by calling the initialisation function
+        // This will configure the device and start streaming
+        if ((err = sdrplay_api_Init(chosenDevice->dev, &cbFns, NULL)) != sdrplay_api_Success)
+        {
+            printf("sdrplay_api_Init failed %s\n", sdrplay_api_GetErrorString(err));
+            sdrplay_api_ErrorInfoT *errInfo = sdrplay_api_GetLastError(NULL);
+            if (errInfo != NULL)
+                printf("Error in %s: %s(): line %d: %s\n", errInfo->file, errInfo->function, errInfo->line, errInfo->message);
+            sdrplay_api_Close();
+            return 0;
         }
     }
     
@@ -167,7 +286,10 @@ int init_SDRplay()
 // clean up SDRplay device
 void remove_SDRplay()
 {
-    mir_sdr_Uninit();
+    printf("Close SDRplay hardware\n");
+
+    sdrplay_api_ReleaseDevice(chosenDevice);
+    sdrplay_api_Close();
 }
 
 void setTunedQrgOffset(int hz)
@@ -177,24 +299,36 @@ void setTunedQrgOffset(int hz)
     double off = (double)hz;
     lastsdrqrg = lastsdrqrg - off;
     printf("set tuner: new:%f offset:%f\n",lastsdrqrg,off);
-    mir_sdr_SetRf(lastsdrqrg,1,0);
+
+    deviceParams->rxChannelA->tunerParams.rfFreq.rfHz = lastsdrqrg;
+
+    if ((err = sdrplay_api_Update(chosenDevice->dev, 
+                                chosenDevice->tuner,
+                                sdrplay_api_Update_Tuner_Frf, 
+                                sdrplay_api_Update_Ext1_None)) != sdrplay_api_Success)
+    {
+        printf("sdrplay_api_Update reset_Qrg_SDRplay failed: %s\n",sdrplay_api_GetErrorString(err));
+    }
+
     printf("set tuner : %.6f MHz\n",lastsdrqrg/1e6);
 }
 
 void reset_Qrg_SDRplay()
 {
-    int gRdBsystem;
-    mir_sdr_ErrT r;
-    lastsdrqrg = tuned_frequency;
-    
-    printf("re-tune: %.6f MHz\n",lastsdrqrg/1e6);
-    
-    r = mir_sdr_Reinit(&gainR, 0, lastsdrqrg/1e6, 0, 0, 0, 0,&gRdBsystem, 0, &samplesPerPacket,mir_sdr_CHANGE_RF_FREQ);
+    sdrplay_api_ErrT err;
 
-	if (r != mir_sdr_Success) {
-		printf("Failed to restart SDRplay RSP device: %d\n",r);
-		return;
-	}
+    lastsdrqrg = tuned_frequency;
+    printf("re-tune: %.6f MHz\n",lastsdrqrg/1e6);
+
+    deviceParams->rxChannelA->tunerParams.rfFreq.rfHz = lastsdrqrg;
+
+    if ((err = sdrplay_api_Update(chosenDevice->dev, 
+                                chosenDevice->tuner,
+                                sdrplay_api_Update_Tuner_Frf, 
+                                sdrplay_api_Update_Ext1_None)) != sdrplay_api_Success)
+    {
+        printf("sdrplay_api_Update reset_Qrg_SDRplay failed: %s\n",sdrplay_api_GetErrorString(err));
+    }
 }
 
 /*
@@ -203,46 +337,34 @@ void reset_Qrg_SDRplay()
  * xi and xq ... samples
  * */
 
-void streamCallback(short *xi, short *xq, unsigned int firstSampleNum,
-    int grChanged, int rfChanged, int fsChanged, unsigned int numSamples,
-    unsigned int reset, unsigned int hwRemoved, void *cbContext)
-{   
-    /*
-     * Testfunction to measure the sample rate 
-     * give it one minute for measurement
-     * 
-     * uncomment this function to check if the SDR hardware 
-     * delivers samples in the right speed
-     */
-    /*
-    struct timeval  tv;
-    static unsigned long lastus=0;
-    static unsigned long samples = 0;
-    static int f=1;
-	gettimeofday(&tv, NULL);
-    if(f)
-    {
-        f = 0;
-        lastus = tv.tv_sec * 1000000 + tv.tv_usec;
-    }
-    samples += numSamples;
-    unsigned long tnow = tv.tv_sec * 1000000 + tv.tv_usec;
-    printf("%.2f\n",((samples*1e6)/(tnow-lastus))/1e6);
-    //return;
-    */
+void StreamACallback(short *xi, short *xq, sdrplay_api_StreamCbParamsT *params, unsigned int numSamples, unsigned int reset, void *cbContext)
+{
+    if (reset)
+        printf("sdrplay_api_StreamACallback: numSamples=%d\n", numSamples);
     
-    // call the function to process the new samples (file: sampleprocessing.c)
 #ifdef WIDEBAND
 	wb_sample_processing(xi, xq, numSamples);
 #else
     fssb_sample_processing(xi, xq, numSamples);
 #endif // WIDEBAND
-}
-
-// currently not used
-void gainCallback(unsigned int gRdB, unsigned int lnaGRdB, void *cbContext)
-{
+    
     return;
 }
+
+void StreamBCallback(short *xi, short *xq, sdrplay_api_StreamCbParamsT *params, unsigned int numSamples, unsigned int reset, void *cbContext)
+{
+    if (reset)
+        printf("sdrplay_api_StreamBCallback: numSamples=%d\n", numSamples);
+    
+    // Process stream callback data here
+    
+    return;
+}
+
+void EventCallback(sdrplay_api_EventT eventId, sdrplay_api_TunerSelectT tuner,
+sdrplay_api_EventParamsT *params, void *cbContext)
+{
+}
+
 
 #endif // SDR_PLAY
